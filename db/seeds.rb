21fb7bf432c5932049442941e4e8ca4be0ef3bcd
“kazuhikochikae” <kazuhikochikae@gmail.com>
2023-12-23 00:14:26 +0900

    ヘロク前

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,48 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(
-   email: '100000@test.com',
-   name: '100000太郎',
+  name: '一般ユーザー',
+  email: 'user@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: User.roles[:user] # ユーザーの役割
+)
+
+User.create!(
+  name: '一般ユーザー02',
+  email: 'user01@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: User.roles[:user] # ユーザーの役割
+)
+User.create!(
+  name: '一般ユーザー03',
+  email: 'user02@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: User.roles[:user] # ユーザーの役割
+)
+
+User.create!(
+  name: 'キャラクター',
+  email: 'character@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: User.roles[:vr_person] # キャラクターの役割
+)
+
+User.create!(
+  name: 'キャラクター02',
+  email: 'character02@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: User.roles[:vr_person] # キャラクターの役割
+)
+
+User.create!(
+  name: 'admin_user',
+  email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: User.roles[:admin] # 管理者
 )
